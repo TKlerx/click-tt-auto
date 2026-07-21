3ee9d4d07969d717470dab6be2177db6e1220679
Timo Klerx <tklerx@paiqo.com>
2026-07-21 10:47:57 +0200

    Rename match review action

--- webapp/src/components/raster/match-review-panel.tsx
+++ webapp/src/components/raster/match-review-panel.tsx
@@ -96,7 +96,7 @@ export function MatchReviewPanel({
                   onClick={() => void mark([record.recordId])}
                   type="button"
                 >
-                  Review
+                  Mark reviewed
                 </button>
               ) : null}
             </div>
